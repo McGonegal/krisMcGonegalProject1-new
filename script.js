2e3388501a35a9a7b9958c30845924ad72e3d57e
krisMcGonegal <krismcgonegal@gmail.com>
2023-06-06 14:18:06 -0400

    Added some super simple js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const displayButton = document.getElementById('display');
+
+const hidden = document.getElementById('hiddenMessage');
+
+displayButton.addEventListener('click', showFunction);
+
+function showFunction (){
+    if (hidden.className = ".hidden"){
+        hidden.className = ".show";
+    }
+}
+
 const button = document.getElementById('popupButton');
 
 button.addEventListener('click', displayFunction);
