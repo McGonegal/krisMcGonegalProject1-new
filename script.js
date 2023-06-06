aa5d017a67daa7d8bc769b3fc1ac71be712c4888
krisMcGonegal <krismcgonegal@gmail.com>
2023-06-06 13:57:22 -0400

    Added alert message to button

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('popupButton');
+
+button.addEventListener('click', displayFunction);
+
+function displayFunction(){
+    alert("More content coming soon!")
+}
+
